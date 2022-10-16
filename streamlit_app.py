2cdf9fd73a8b0602b95f63ded65713a5f210a2e6
Sai Sri Ram Tammineedi <115405508+sriramlpu@users.noreply.github.com>
2022-10-16 18:21:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
